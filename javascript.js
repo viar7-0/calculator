f2eaa4b999e7e3c3cf6dacbcdadf0996754a1e75
viar7-0 <vitiarora7@gmail.com>
2026-06-25 00:22:49 +0530

    Fix decimal point

--- javascript.js
+++ javascript.js
@@ -14,9 +14,11 @@ function divide(a, b) {
 let num1 = '', sign, num2 = '', ans
 
 function operate(first, sign, second) {
+    
     a = Number(first)
     b = Number(second)
-    if (sign == '+') {
+ 
+     if (sign == '+') {
         ans = add(a, b)
     }
     else if (sign == '-') {
@@ -29,16 +31,20 @@ function operate(first, sign, second) {
         ans = divide(a, b)
     }
 }
+
 function update() {
     const numClick = document.querySelectorAll(".btn")
     const display = document.querySelector('.display')
     const opClick = document.querySelectorAll('.one')
     k = ''
     f = false
+    deci=false
+    deci2=false
     numClick.forEach(button => {
 
 
         button.addEventListener("click", function (event) {
+           
             
             t=this.textContent
             if(t=='clear'){
@@ -46,24 +52,50 @@ function update() {
                 sign=undefined
                 num2=''
                 display.textContent=''
+                deci=false
+                deci2=false
+                 
             }
+            
             else{
 
             if (!f) {
 
-                if (num1 != 'clear') {
+                if (t != '.') {
                     num1 += this.textContent
                     display.textContent = num1
                 }
+                else {
+                    if(!deci){
+                        deci=true
+                         num1 += this.textContent
+                    display.textContent = num1
+                
+                    }
+                    
+                    
+                }
             }
             else {
+                 
 
-                if (num2 != 'clear') {
+                if (t!='.') {
                 
                     num2 += this.textContent
                     display.textContent = num2
                     operate(num1, sign, num2)
                    
+                }
+                else{
+                      if(!deci2){
+
+                        deci2=true
+                         num2 += this.textContent
+                    display.textContent = num2
+                    
+                    }
+                    
+
                 }
             }
         }
@@ -84,16 +116,20 @@ function update() {
                 display.textContent=ans
                 num1 = ans
                 num2 = ''
+                deci=false
+                deci2=false
             }
 
            
             if(k=='=' ){
-                if(num2!=''){
+                if(num2!='' && ans!=Infinity){
                 display.textContent = ans 
                 num1=ans
                 num2=''
                 sign=undefined
                 f=true
+                deci=false
+                deci2=false
                 }
                 else {
                     display.textContent="ERR"
@@ -101,6 +137,8 @@ function update() {
                     num1 = ''
                     sign = undefined
                     num2 = ''
+                    deci=false
+                    deci2=false
                 }
                
             }
